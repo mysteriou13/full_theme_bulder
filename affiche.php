7f02953f1c5945380ede283f03bbef1b91852bfb
mysteriou <elio007@hotmail.fr>
2022-03-26 17:56:50 +0100

    ajout fonction affiche_liste_post

--- affiche.php
+++ affiche.php
@@ -22,7 +22,50 @@ class afficher extends \data\sql{
        
     }
    
+    function affiche_liste_post($el_ligne){
 
+   $count_post = count(get_posts());
+
+   $tab = [];
+
+   $c = 0;
+
+    for($c == 0; $c < $count_post; $c++){
+
+       array_push($tab,get_posts()[$c]->post_content);
+
+    }
+
+    $l = array_chunk($tab, $el_ligne);
+
+     $n = 0;
+
+     for($n == 0; $n <= count($l)-1; $n++){
+
+      $n1 = 0;
+
+     $c =  count($l[0]);
+
+     echo "<div class = 'd-flex justify-content-between text-light' >";
+     for($n1 == 0; $n1 <= $c; $n1++){
+
+  echo "<div>".$l[$n][$n1]."</div>";
+
+     }
+
+     echo "</div>";
+ 
+     }
+    
+
+}
+    
+
+    
+
+
+
+        
 
 }
    
