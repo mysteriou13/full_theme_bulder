8928ea586b719b37bb1765b73e6fcf8592d2fa73
mysteriou <elio07@hotmail.fr>
2022-10-30 06:48:37 +0100

    ajout class dans l'index

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php 
 
+ require_once "sql.php";
+
+ require_once "affiche.php";
+
+ require_once "header.php";
+
+ require_once "mail.php";
+
+ if(file_exists($full_theme_hiki."./config.php")){
+   
+    $mail = new \mails\mail(true);
+
+   }
+
+
+   $sql = new \data\sql();
+
+
+ $sql = new \data\sql();
+   
+ $affiche = new \affiche\afficher();
+
+ $menu_header = new \menu_header\menu_header();
+
+
 ?>
