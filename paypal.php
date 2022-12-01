d84bcf15536ff4288d3ba82e6475f939f1cf0d32
mysteriou <elio07@hotmail.fr>
2022-12-01 23:02:06 +0100

    ajout function button_paypal buy_now dans la class paypal

--- paypal.php
+++ paypal.php
@@ -63,9 +63,6 @@ echo "</td> </tr>";
 
 echo "<tr>  <td> prix unitaire : </td> <td>";
 
-
-echo $r[$a]->prix;
-
 echo "</td>
 
    </tr>";
@@ -76,10 +73,52 @@ echo "</table>";
 
 echo $total =  array_sum($prix);
 
+$this->button_paypal($total);
         
     }
 
-}
 
+    function button_paypal($total){
+
+echo '
+        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_xclick">
+              <input type="hidden" name="business" value="<?php echo $email_business?>">
+              <input type="hidden" name="item_name" value="<?php echo $item_name;?>">
+              <input type="hidden" name="item_number" value="<?php echo $number;?>">
+              <input type="hidden" name="amount" value="<?php echo $total;?>">
+              <input type="hidden" name="shipping" value="2">
+              <input type="hidden" name="no_note" value="1">
+              <input type="hidden" name="currency_code" value="EUR">
+              <input type="hidden" name="lc" value="AU">
+              <input type="hidden" name="bn" value="PP-BuyNowBF">
+            <input type="hidden" name="hosted_button_id" value="6RNT8A4HBBJRE">
+          
+            <div>
+          
+            <div>
+              
+             </div>
+          
+             <div>
+           
+             total';
+              echo $total." &nbsp;EUR";
+
+             echo '
+              </div>
+          
+          
+            </div>
+          
+            <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_buynow_107x26.png" alt="Buy Now">
+            <img alt="" src="https://paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>
+';
+
+
+    }
+
+}
 
 ?>
